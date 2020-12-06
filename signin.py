a69300b9912c98ad5d2c64bcb32e0e581751ff42
290149290 <290149290@qq.com>
2020-12-06 16:39:58 +0800

    Update signin.py

--- signin.py
+++ signin.py
@@ -1,5 +1,5 @@
 import time
-
+import os
 
 def save(file, res):
     with open(file, 'w', encoding='utf-8') as f:
@@ -7,8 +7,10 @@ def save(file, res):
 
 
 def start():
+    print(time.ctime(),"要写文件了,不知道能否成功")
     save("hello.txt", "world" + time.ctime())
-    print(time.ctime())
+    print(time.ctime(),"这里是写完了")
+    print(os.path.isfile("hello.txt"))
 
 
 if __name__ == '__main__':
