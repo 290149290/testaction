ba20456aab9b481f16a204ef54b05fb190a20a7d
290149290 <290149290@qq.com>
2020-12-06 16:43:02 +0800

    Update signin.py

--- signin.py
+++ signin.py
@@ -1,16 +1,28 @@
 import time
 import os
 
+
 def save(file, res):
     with open(file, 'w', encoding='utf-8') as f:
         f.write(res)
 
 
+def 文件_读入文本(file):
+    try:
+        with open(file, mode="r", encoding='utf-8') as f:
+            return f.read()
+    except UnicodeDecodeError:
+        # print('检测到编码可能是gbk,正在调用gbk...请稍候...')
+        with open(file, mode="r", encoding='gbk') as f:
+            return f.read()
+
+
 def start():
-    print(time.ctime(),"要写文件了,不知道能否成功")
+    print(time.ctime(), "要写文件了,不知道能否成功")
     save("hello.txt", "world" + time.ctime())
-    print(time.ctime(),"这里是写完了")
+    print(time.ctime(), "这里是写完了")
     print(os.path.isfile("hello.txt"))
+    print(文件_读入文本("hello.txt"))
 
 
 if __name__ == '__main__':
