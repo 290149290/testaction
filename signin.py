f5202b4cde227fe005408210b11a362ac7a933c8
290149290 <290149290@qq.com>
2020-12-06 17:55:51 +0800

    Update signin.py

--- signin.py
+++ signin.py
@@ -19,7 +19,7 @@ def 文件_读入文本(file):
 
 def start():
     print(time.ctime(), "要写文件了,不知道能否成功")
-    save("hello.txt", "world" + time.ctime())
+    save("hello.txt", "nowtime:" + time.ctime())
     print(time.ctime(), "这里是写完了")
     print(os.path.isfile("hello.txt"))
     print(文件_读入文本("hello.txt"))
