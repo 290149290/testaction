9aefddb84b591a1077ed3a46755711b62b4f4037
290149290 <290149290@qq.com>
2020-12-06 15:46:13 +0800

    Add files via upload

---
+++ signin.py
@@ -0,0 +1,12 @@
+import time
+
+def save(file, res):
+    with open(file, 'w', encoding='utf-8') as f:
+        f.write(res)
+
+def start():
+    save("hello.txt","world"+time.ctime())
+
+if __name__ == '__main__':
+  start()
+
