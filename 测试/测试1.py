297b513cc8fbac2bc4f0255b378aa57d9ee0646b
290149290 <290149290@qq.com>
2020-12-08 00:08:49 +0800

    2020年12月8日00:08:47

--- 测试/测试1.py
+++ 测试/测试1.py
@@ -7,10 +7,9 @@ s = requests.Session()  # 创建 requests.Session()对象s
 s.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:32.0) Gecko/20100101 Firefox/32.0"}
 # 集成header
 
-# url = 'https://www.aaa.com'
 url = 'https://webapi.sporttery.cn/gateway/lottery/getHistoryPageListV1.qry?gameNo=85&provinceId=0&pageSize=30&isVerify=1&pageNo=1'
-url = 'https://www.cnblogs.com/wangchuang/p/11568423.html'
-url = 'http://kaijiang.zhcw.com/zhcw/html/ssq/list.html'
+# url = 'https://www.cnblogs.com/wangchuang/p/11568423.html'
+# url = 'http://kaijiang.zhcw.com/zhcw/html/ssq/list.html'
 r = s.get(url, verify=False)  # 使用该对象进行get/post操作  # session已经包含了headers参数 # 禁止ssl 解决https
 
 print(r.text)
@@ -19,4 +18,5 @@ def save(file, res):
     with open(file, 'w', encoding='utf-8') as f:
         f.write(res)
 
-save("2020年12月7日_232100.txt", r.text)
+# save("2020年12月7日_232100.txt", r.text)
+save("2020年12月8日.txt", r.json)
