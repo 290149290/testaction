56b86b88caa6ff63b1917132193633cb5b73df49
290149290 <290149290@qq.com>
2020-12-09 03:43:11 +0800

    2020年12月9日03:42:58 获取ip 修改re正则表达式

--- 获取ip.py
+++ 获取ip.py
@@ -7,7 +7,8 @@ def getIP():
     headers['User-Agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36"
     response = requests.get('http://www.baidu.com/s?wd=ip',headers=headers)
     html = response.text
-    reGET = re.compile('fk="(.*?)"').findall(html)
+    # reGET = re.compile('fk="(.*?)"').findall(html)
+    reGET = re.compile('ip地址(.*?) ').findall(html)
     for i in reGET:
         print('外网地址： %s'%i)
 getIP()
